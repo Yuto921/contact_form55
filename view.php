7eba2ba72593f3011ad9f01185bbad5f7dd818f5
久松勇斗 <>
2019-08-30 16:47:46 +0900

    完成！

--- view.php
+++ view.php
@@ -1,3 +1,31 @@
+<?php
+    // 目的 : 画面にお問い合わせの一覧を出す
+    // [プロセス]
+    // 1. 必要な部品を読み込む(functions.php , dbconnect.php)
+    // 2. 画面に出力するものを取得する(SELECT文)
+    // 3. 取得したデータを画面に表示する
+
+
+    // 必要な部品を読み込む
+    require_once('functions.php');
+    require_once('dbconnect.php');
+
+    // 画面に出力するものを取得する
+    // SELECT文の準備
+    $stmt = $dbh->prepare('SELECT * FROM surveys ');
+    // 準備したものを実行 //もし、 ? の部分に当たる値を配列で渡す
+    $stmt->execute();
+
+    // 取得した一覧を変数に格納 fetchAll(全件の検索結果)
+    $results = $stmt->fetchAll();
+
+    // var_dump($results); //データベースからの値を [配列] で返す  キーと値のセット
+
+    // 検索結果が[配列]になって入ってきている -> foreachで回して一個の値を取得
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,7 +35,22 @@
     <title>お問い合わせ一覧</title>
 </head>
 <body>
-    
-    
+
+    <h1>一覧</h1>
+
+    <!-- 一覧を表示する -->
+    <?php foreach ($results as $result) { ?>
+
+        <p>名前: <?php echo h($result['username']); ?></p>
+        <p>メールアドレス: <?php echo h($result['email']); ?></p>
+        <p>内容: <?php echo h($result['content']); ?></p>
+
+        <!-- hrタグ 線を引く -->
+        <hr>
+
+    <?php } ?>
+
+
+
 </body>
 </html>
